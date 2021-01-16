b7d0010fec935f89eba2421bd6ef3eb49c1849c7
Boon Boonsiri <35120291+boonboonsiri@users.noreply.github.com>
2021-01-16 18:17:42 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+import discord
+import pandas as pd
+
+client = discord.Client()
+guild = discord.Guild
+
+def readPrevMessage():
+	channels = client.get_all_channels();
+
+	for c in channels:
+		for m in c.messages(limit=200):
+			print(m)
+
+@client.event
+
+async def on_message(message):
+	if message.author == client.user: # in case of discord bot
+		return
+
+	# we're going to have it read everything
+	# we'll need another if here for your user
+	message1 = message.content;
+	author1 = message.author;
+	channel = message.channel;
+	time = message.created_at
+
+
+	if message.content.startswith('dasamoodreg'):
+		username = message.content.split()[0].replace("dasamoodreg","")
+		username = message.content.split()[1:][0]
+		print(username)
+		readPrevMessage();
+		
+		await channel.send(username)
+
+
+
+	print(message1)
+	print(author1)
+	print(channel)
+	print(time)
+	print("")
+
+client.run('Nzk5ODUwNjE2OTI2NTAyOTYz.YAJk7w.DvRCd9HHhjJ9SwO0sK4Nja3zvII');
